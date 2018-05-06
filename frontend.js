19a6ec63036793b4ff9bd98180159f59b560e4fe
jentino <jentino@gmail.com>
2018-05-07 00:49:35 +0200

    asdf1

--- frontend.js
+++ frontend.js
@@ -13,7 +13,7 @@ $(function () {
     var ws = new WebSocket(HOST);
     var el = document.getElementById('server-time');
     ws.onmessage = function (event) {
-    el.innerHTML = 'Server time: ' + event.data;
+        el.innerHTML = 'Server time: ' + event.data;
     };
 
     // var timer = $('#servertime');
