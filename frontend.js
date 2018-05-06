b8a4455a9e5addfb030d3f6b106fe245314f6d5d
jentino <jentino@gmail.com>
2018-05-07 00:35:06 +0200

    asdfq

--- frontend.js
+++ frontend.js
@@ -1,14 +1,12 @@
 
 
 $(function () {
-    "use strict";
     
-      var HOST = location.origin.replace(/^http/, 'ws')
-      var ws = new WebSocket(HOST);
-      var el = document.getElementById('server-time');
-      ws.onmessage = function (event) {
-        el.innerHTML = 'CurrentTime: ' + event.data;
-      };
-
+    var HOST = location.origin.replace(/^http/, 'ws')
+    var ws = new WebSocket(HOST);
+    var el = document.getElementById('server-time');
+    ws.onmessage = function (event) {
+    el.innerHTML = 'CurrentTime: ' + event.data;
+    };
 
  });
